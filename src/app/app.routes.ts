b1c4c2de9d95f4acf3a4fa8d145dec0d63757ae0
ameena-nia <155574888+ameena-nia@users.noreply.github.com>
2024-11-06 17:29:31 -0500

    New components
    Created new dashboard component and user-goals component.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { MainComponent } from './main/main.component';
+//import { MainComponent } from './main/main.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserGoalsComponent } from './user-goals/user-goals.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent }, // Login page route
-  { path: 'main', component: MainComponent } // Main landing page route
+  //{ path: 'main', component: MainComponent }, // Main landing page route
+  { path: 'dashboard', component: DashboardComponent }, // Main landing page route
+  { path: 'user-goals', component: UserGoalsComponent } // Goals page route
 ];
---
+++ src/app/user-goals/user-goals.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-user-goals',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-goals.component.html',
+  styleUrl: './user-goals.component.css'
+})
+export class UserGoalsComponent {
+
+}
