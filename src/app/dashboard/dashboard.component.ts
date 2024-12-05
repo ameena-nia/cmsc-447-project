19616d1d7a2349308cd07781c807f6388e2d35bb
ameena-nia <155574888+ameena-nia@users.noreply.github.com>
2024-12-05 01:01:31 -0500

    Progress of dialog popup. "Add Goal" button functionality working.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  isDialogOpen: boolean = false; // Dialog Visibility
 
+  // Function to open the dialog
+  openDialog(): void{
+    this.isDialogOpen = true;
+  }
+
+  // Function to close the dialog
+  closeDialog(): void{
+    this.isDialogOpen = false;
+  }
 }
