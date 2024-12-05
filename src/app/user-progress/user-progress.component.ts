8e2c437543256fb38b27fcf386b944fc4cfe0283
Aisosa Aimufua <aisosaaimufua@gmail.com>
2024-12-04 22:19:02 -0500

    Added components for dashboard support and fixed routing

---
+++ src/app/user-progress/user-progress.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-progress',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-progress.component.html',
+  styleUrl: './user-progress.component.css'
+})
+export class UserProgressComponent {
+
+}
